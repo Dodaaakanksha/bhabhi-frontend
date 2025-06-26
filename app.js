374bb060e3526a391e3e06c0cec115b9fc2c8dda
Dodaaakanksha <Dodaaakanksha@users.noreply.github.com>
2025-06-26 15:44:13 +0530

    Correct the element name.

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const SUPABASE_URL = "https://oyuqmsfifmlrxawsrtwl.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im95dXFtc2ZpZm1scnhhd3NydHdsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA5MzA0NzcsImV4cCI6MjA2NjUwNjQ3N30.MhKu34EGcP6h7afU066a_haib7JfTxurrYz4cDPviYc";
 
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 document.getElementById('game').innerHTML = '<p>Game UI will go here</p>';
-
-// You will build this part gradually to connect players, show cards etc.
