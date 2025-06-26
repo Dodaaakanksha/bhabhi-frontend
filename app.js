4a68d45b9cd1c2b278998e80327acbce85627ae8
Dodaaakanksha <Dodaaakanksha@users.noreply.github.com>
2025-06-26 15:46:59 +0530

    Wait for loading supabase

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-const SUPABASE_URL = "https://oyuqmsfifmlrxawsrtwl.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im95dXFtc2ZpZm1scnhhd3NydHdsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA5MzA0NzcsImV4cCI6MjA2NjUwNjQ3N30.MhKu34EGcP6h7afU066a_haib7JfTxurrYz4cDPviYc";
+window.onload = () => {
+  const SUPABASE_URL = "https://oyuqmsfifmlrxawsrtwl.supabase.co";
+  const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im95dXFtc2ZpZm1scnhhd3NydHdsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA5MzA0NzcsImV4cCI6MjA2NjUwNjQ3N30.MhKu34EGcP6h7afU066a_haib7JfTxurrYz4cDPviYc";
 
-const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+  const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
-document.getElementById('game').innerHTML = '<p>Game UI will go here</p>';
+  document.getElementById('game').innerHTML = '<p>Game UI will go here</p>';
+};
